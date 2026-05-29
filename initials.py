4fc0f48e7a106815dbdd65783b399f69c8f65589
Maxwell1174 <2425401174@live.gctu.edu.gh>
2026-05-29 20:33:19 +0000

    Update initials.py

--- initials.py
+++ initials.py
@@ -1,7 +1,19 @@
 
 def main():
-  pass #code goes here
-
+  pass #coprint()
+print("...MAA")
+print()
+print("...MMMM....MMMM.........AAAAA..........AAAAA.....")
+print("...MMMM....MMMM........AAAAAAA........AAAAAAA....")
+print("...MM.MM..MM.MM.......AA.....AA......AA.....AA...")
+print("...MM.MM..MM.MM......AA.......AA....AA.......AA..")
+print("...MM.MM..MM.MM......AAAAAAAAAAA....AAAAAAAAAAA..")
+print("...MM..MMMM..MM......AAAAAAAAAAA....AAAAAAAAAAA..")
+print("...MM..MMMM..MM......AA.......AA....AA.......AA..")
+print("...MM...MM...MM......AA.......AA....AA.......AA..")
+print("...MM...MM...MM..**..AA.......AA..**AA.......AA**")
+print("...MM...MM...MM..**..AA.......AA..**AA.......AA**")
+print()de goes here
 
 
 main()
